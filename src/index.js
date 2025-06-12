235f5b61b6deaa4dec042b3342924259afb611cf
Doker367 <blcdoker@gmail.com>
2025-06-12 09:39:16 -0600

    primer avance LARCAD

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Punto de entrada principal para la app Node.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('src/public'));
+
+// Importar rutas
+// const routes = require('./routes');
+// app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
